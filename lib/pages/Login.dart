4519547552acf2ffef9b84d5841d58f60890cd12
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-03-27 15:51:26 +0600

    link search page done

---
+++ lib/pages/Login.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      
+      body:
+      Container(
+          height: double.infinity,
+          color: Colors.black,
+        child: Center(
+          child: SizedBox(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 1),
+                                  borderRadius: BorderRadius.circular(10),),
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,width: 2),borderRadius: BorderRadius.circular(10)),
+                                border: OutlineInputBorder(
+                                  
+                                ),
+                                hintText: 'Email',hintStyle: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 1),
+                                  borderRadius: BorderRadius.circular(10),),
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,width: 2),borderRadius: BorderRadius.circular(10)),
+                                border: OutlineInputBorder(
+                                  
+                                ),
+                                hintText: 'Password',hintStyle: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 70,
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text('Login',
+                                      style: TextStyle(
+                                          fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
+                                          ),
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: Color(0xffF11842),
+                                            side: BorderSide(color: Color(0xffF11842), width: 2),
+                                          ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        
+                        Container(
+                          margin: EdgeInsets.only(top: 15),
+                          child: Text("Forgot Password?", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),),
+                          ),
+                          SizedBox(
+                            height: 1,
+                            width: 120,
+                            child: 
+                            Container(
+                              color: Colors.white,
+                            )
+                            ,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/admins.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+
+
+class Admins extends StatelessWidget {
+  const Admins({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+  var searchListView = [];
+  String dropdownValue = 'red';
+    return 
+    Scaffold(
+      body: 
+      SingleChildScrollView(
+        child: Container(
+          child:
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: Text("Enter your link here", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: Align(
+                    widthFactor: 1.0,
+                    heightFactor: 1.0,
+                    child: Icon(
+                      Icons.search,
+                    ),),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(10),),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black,width: 2),borderRadius: BorderRadius.circular(10)),
+                  border: OutlineInputBorder(
+                    
+                  ),
+                  hintText: 'https://example.com',hintStyle: TextStyle(color: Color.fromARGB(105, 0, 0, 0)),
+                ),
+                ),
+              
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ListView(
+                  shrinkWrap: true,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffF5F5F5))
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset('assets/images/user.jpg', height: 40, width: 40,),
+                              Text("Tiltes titles titles", style: TextStyle(color: Colors.black),),
+                            ],
+                          ),
+                        ),
+                      ),
+      
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffF5F5F5))
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset('assets/images/user.jpg', height: 40, width: 40,),
+                              Text("Tiltes titles titles", style: TextStyle(color: Colors.black),),
+                            ],
+                          ),
+                        ),
+                      ),
+      
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffF5F5F5))
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset('assets/images/user.jpg', height: 40, width: 40,),
+                              Text("Tiltes titles titles", style: TextStyle(color: Colors.black),),
+                            ],
+                          ),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+              ),
+                
+              
+              Container(
+                margin: EdgeInsets.only(top: 40, bottom: 20),
+                child: 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Categories", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 16),),
+                    ElevatedButton(onPressed: () {
+                      
+                    }, child: 
+                    Text("Choose"),
+                    ),
+                  ],
+                ),
+              ),
+      
+              
+                    
+              ListView(
+                shrinkWrap: true,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Fashion", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Sports", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Politics", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                  ],
+                ),
+      
+      
+      
+      
+                Container(
+                margin: EdgeInsets.only(top: 40, bottom: 20),
+                child: 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Topics", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 16),),
+                    ElevatedButton(onPressed: () {
+                      
+                    }, child: 
+                    Text("Choose"),
+                    ),
+                  ],
+                ),
+              ),
+      
+              
+                    
+              ListView(
+                shrinkWrap: true,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Topic-1", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Topic-2", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Color(0xffF5F5F5))
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Topic-3", style: TextStyle(color: Colors.black),),
+                            IconButton(
+                            icon: const Icon(Icons.delete,color: Colors.red,),
+                            onPressed: () {
+                              
+                            },
+                          ),
+                          ],
+                        ),
+                      ),
+                    ),
+      
+                  ],
+                ),
+      
+              
+      
+              ],
+            ),
+            
+          ), 
+          
+        ),
+      ),
+    )
+    ;
+  }
+}
